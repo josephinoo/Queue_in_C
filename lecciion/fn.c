bbe3b8bb6a29ba15a810135425ec566e6a913472
Joseph Danilo Avila Alvarez <josdavil@espol.edu.ec>
2020-07-16 18:02:43 -0500

    delate

--- lecciion/fn.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int valor =20;
-void fn(){
-
-    valor=17;
-    printf("cha\n");
-}
--- lecciion/leccion.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void fn();
-int valor =9;
-int main(){
-
-    valor=4;
-    fn();
-    printf("%d\n",valor);
-}
