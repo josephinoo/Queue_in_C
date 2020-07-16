2ec00f4466c74c00d59f6e18384987478cac588c
Joseph Danilo Avila Alvarez <josdavil@espol.edu.ec>
2020-07-16 12:30:11 -0500

    update

---
+++ lecciion/fn.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int valor =20;
+void fn(){
+
+    valor=17;
+    printf("cha\n");
+}
---
+++ lecciion/leccion.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void fn();
+int valor =9;
+int main(){
+
+    valor=4;
+    fn();
+    printf("%d\n",valor);
+}
